9e68a62a09b0057e15a8c07c82093eb726bf86ca
delphine.millot <delfymillot@gmail.com>
2017-12-16 13:01:39 +0100

    nettoyage des fichiers inutiles

--- anciens fichiers procéduraux/BLOG_RETOUR.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-// Puis rediriger vers commentsList.php comme ceci :
-header('Location: commentsList.php');
-exit;
--- anciens fichiers procéduraux/blog_fin_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-// Puis rediriger vers minichat.php comme ceci :
-header('Location: index.php');
-exit;
