1af37f4f40bcf24c9f5670bc67ded9557a111c04
delphine.millot <delfymillot@gmail.com>
2017-12-17 22:54:28 +0100

    demarrage de la creation de l'admin

---
+++ App/Controller/Admin/Admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: delphinemillotpedrero
+ * Date: 17/12/2017
+ * Time: 22:52
+ */
+namespace App\Controller\Admin;
+
+class Admin
+{
+
+}
--- App/index.php
+++ App/index.php
@@ -5,12 +5,16 @@ require 'Autoload.php';
 \App\Autoload::register();
 
 $page = new App\Controller\ChapterController();
+$user = new App\Controller\Admin\Admin();
+
 try {
     if (isset($_GET['p'])){
         if ($_GET['p'] == 'post') {
             $page->post();
         } elseif (isset($_GET) && !empty($_GET['id'])) {
             $page->chapter(htmlspecialchars($_GET['id']));
+        } elseif ($_GET['p'] == 'login'){
+            $user->login();
         }
 
     } else {
